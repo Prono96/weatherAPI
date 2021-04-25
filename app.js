223432f22e6a05eab9d2b5be3196d1c936dce75d
Promise <promiseprince01@gmail.com>
2021-04-25 17:53:23 +0100

    first commit: Call data from api and it's working fine on console log

--- app.js
+++ app.js
@@ -4,16 +4,22 @@ window.addEventListener("load", () => {
 
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
-      long = position.coords.longitude;
+      lon = position.coords.longitude;
       lat =  position.coords.latitude;
 
+      // Cors for cross origin resources system 
+      // const proxy = "https://cors-anywhere.herokuapp.com/"
+
       // Api key
-      const api = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=4b537fedef0fb24b53173d1ec82cedfe`;
+      const api = `https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=4b537fedef0fb24b53173d1ec82cedfe`;
 
       // Return fetch API
       fetch(api)
       .then((response) => {
         return response.json();
+      })
+      .then(data => {
+        console.log(data);
       })
     })
   }
