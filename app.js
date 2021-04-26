fcead92700cfffb55995fc981aa927eb7bbaee91
Promise <promiseprince01@gmail.com>
2021-04-26 21:35:27 +0100

    Fetching the data from API to browser console now working fine

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 window.addEventListener("load", () => {
   let long; 
   let lat;
+  let locationTimezone = document.querySelector('location-timezone');
+  let TempDegree = document.querySelector('temp-degree');
+  let TempDescription = document.querySelector('temp-description');
 
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
-      lon = position.coords.longitude;
+      long = position.coords.longitude;
       lat =  position.coords.latitude;
 
       // Cors for cross origin resources system 
-      // const proxy = "https://cors-anywhere.herokuapp.com/"
+      const proxy = "https://cors-anywhere.herokuapp.com/"
 
       // Api key
-      const api = `https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=4b537fedef0fb24b53173d1ec82cedfe`;
+      const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=10.306720&lon=9.844930&appid=4b537fedef0fb24b53173d1ec82cedfe`;
 
       // Return fetch API
       fetch(api)
@@ -19,9 +22,17 @@ window.addEventListener("load", () => {
         return response.json();
       })
       .then(data => {
-        console.log(data);
+        
+
+        const {name} = data;
+
+        console.log(name);
+        // const {temp} = data.main.temp;
+
+        // Set DOM element from API
+        // TempDegree.textContent = temp;
       })
-    })
+    })  
   }
 
 
