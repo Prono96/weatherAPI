23dcb58bcc33f6a54849216e27d54bba7b582d83
Promise <promiseprince01@gmail.com>
2021-04-26 23:18:39 +0100

    UI parameters functionality working fine except for weather icon

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 window.addEventListener("load", () => {
   let long; 
   let lat;
-  let locationTimezone = document.querySelector('location-timezone');
-  let TempDegree = document.querySelector('temp-degree');
-  let TempDescription = document.querySelector('temp-description');
+  let locationTimezone = document.querySelector('.location-timezone');
+  let TempDegree = document.querySelector('.temp-degree');
+  let TempDescription = document.querySelector('.temp-description');
+  let Icon = document.querySelector('.icon');
 
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -22,11 +23,39 @@ window.addEventListener("load", () => {
         return response.json();
       })
       .then(data => {
-        
-
         const {name} = data;
+        const {feels_like} = data.main;
+        const {description} = data.weather[0];
+        const {icon} = data.weather[0];
 
-        console.log(name);
+        //Fetching data to UI
+        locationTimezone.textContent = name;
+        TempDegree.textContent = Math.round(feels_like - 273);
+        TempDescription.textContent = description;
+        // Icon.textContent = icon;
+        
+        // Modification of weather Icons 
+        
+        // if(id < 250) {
+        //   Icon.src = './icons/storm.svg'
+        // } else if(id < 350){
+        //   Icon.src = './icons/drizzle.svg'
+        // } else if(id < 550){
+        //   Icon.src = './icons/rain.svg'
+        // } else if(id < 630){
+        //   Icon.src = './icons/snow.svg'
+        // } else if(id < 800){
+        //   Icon.src = './icons/atmosphere.svg'
+        // } else if(id === 800){
+        //   Icon.src = './icons/clear.svg'
+        // } else if(id > 800){
+        //   Icon.src = './icons/clouds.svg'
+        // }
+
+
+
+
+        console.log(data);
         // const {temp} = data.main.temp;
 
         // Set DOM element from API
